11e83576a6ecb7b07b5867b8305e9d60b35cf13c
Semfry <shaunfriend8@gmail.com>
2022-07-06 22:56:32 +0100

    Wed

--- mysite/mypages/views.py
+++ mysite/mypages/views.py
@@ -10,20 +10,26 @@ def index(request):
     return render(request, 'mypages/mainpage.html')
 
 def modspage(request):
+    if request.method == 'POST':
+        modslist_form = modslistform(request.POST, request.FILES)
+        if modslist_form.is_valid():
+            modslist_form.save()
+            messages.success(request, ('Saved successfully!'))
+        else:
+            messages.error(request, ('Error'))
+    
+    modslist_form = modslistform()
     modsnames = modslist.objects.all
-    context = {'mod_name': modsnames}
-    return render(request, 'mypages/mods.html', context)
+    return render(request=request, template_name='mypages/mods.html', context={'modslist_form':modslist_form, 'mod_names':modsnames })
 
 def favouritegames(request):
     if request.method == 'POST':
         favegames_form = favegamesform(request.POST, request.FILES)
-        if favegamesform.is_valid():
-            favegamesform.save()
+        if favegames_form.is_valid():
+            favegames_form.save()
             messages.success(request, ('Saved successfully!'))
         else:
             messages.error(request, ('Error'))
-
-        return redirect("mypages/favouritegames.html")
     
     favegames_form = favegamesform()
     gamenames = favegames.objects.all
