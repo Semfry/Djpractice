cbc99622504c73094afd2b77fe1f6ad0212bd725
Semfry <shaunfriend8@gmail.com>
2022-07-20 01:56:50 +0100

    wed20072022

--- mysite/mypages/models.py
+++ mysite/mypages/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-import datetime
 
 # Create your models here.
 
 class favegames(models.Model):
     gamename = models.CharField("name of game/series", max_length=100)
-    startyear = models.DateField("release year/start of series")
+    startyear = models.DateField("release year/start of series (format DD/MM/YY)")
 
     def __str__(self):
         return self.gamename
@@ -14,7 +13,7 @@ class favegames(models.Model):
 
 class modslist(models.Model):
     modname = models.CharField("name of mod", max_length=100)
-    releaseyear = models.DateField("release year/start of series")
+    releaseyear = models.DateField("release year/start of series (format DD/MM/YY)")
     game = models.CharField("name of game", max_length=100)
     link = models.URLField(max_length=200, default="admin/mypages/mods/")
     image = models.ImageField(upload_to="static/images")
